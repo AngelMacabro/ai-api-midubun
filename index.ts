7547734cd8c616ba7c132ad2916a21509c9ad0d6
AngelMacabro <87783824+AngelMacabro@users.noreply.github.com>
2025-12-30 21:51:03 -0300

    refactor: update endpoint response and log text to API OK
    - Replace fetch response text from "Hello World!" to "API OK!"
    - Update console log to "API OK! -> Listening on http://localhost:${server.port}"

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ const port = Number(process.env.PORT) || 3000;
 const server = Bun.serve({
   port,
   async fetch(req) {
-    return new Response("Hello World!");
+    return new Response("API OK!");
   },
 });
 
-console.log(`Listening on http://localhost:${server.port}`);
+console.log(`API OK! -> Listening on http://localhost:${server.port}`);
